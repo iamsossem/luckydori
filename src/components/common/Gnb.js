cfb90b9da41565b1c0d57d6ef91805b9ebc9ceb8
iamsossem <iamsossem@gmail.com>
2025-12-16 17:57:20 +0900

    init

--- src/components/common/Gnb.js
+++ src/components/common/Gnb.js
@@ -1,7 +1,38 @@
-const Gnb = () => {
-  return (
-    <nav>Gnb</nav>
-  )
-}
+import { useState } from "react";
+import "./Gnb.scss";
+import { MdOutlineMenu } from "react-icons/md";
+import { MdClose } from "react-icons/md";
 
-export default Gnb
+const Gnb = () => {
+  const [isShow, setIsShow] = useState(false);
+  const handleClick = () => {
+    setIsShow(!isShow);
+  };
+  return (
+    <nav>
+      <div className="mobile-toggle" onClick={handleClick}>
+        <button>{isShow ? <MdClose /> : <MdOutlineMenu />}</button>
+        <span>감성별 추천</span>
+      </div>
+      {isShow && (
+        <div className="menu-wrap">
+          <ul className="menu-top">
+            <li>전체보기👀</li>
+            <li>인기박스💖</li>
+            <li>신상박스🌹</li>
+            <li>다시 찾아온 문구들🎁</li>
+          </ul>
+          <ul className="menu-bottom">
+            <li>설렘박스💌</li>
+            <li>집중박스📚</li>
+            <li>힐링박스🌿</li>
+            <li>우정박스🤝</li>
+            <li>다꾸박스🎀</li>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Gnb;
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,9 +1,15 @@
 import Gnb from "./Gnb"
+import logoImg from '../../assets/images/logo.png'
+import { MdShoppingCartCheckout } from "react-icons/md"
+import "./Header.scss"
 
 const Header = () => {
   return (
     <header>
-      Header
+      <div className="header-top">
+        <img src={logoImg} alt="luckydori 로고 이미지" />
+        <MdShoppingCartCheckout className="cart-icon"/>
+      </div>
       <Gnb />
     </header>
   )
