eee58706bb4476ae3180bab3375a0cedd758df8b
iamsossem <iamsossem@gmail.com>
2025-12-17 17:22:44 +0900

    2025.12.17 update

--- src/components/main/ProductSection.js
+++ src/components/main/ProductSection.js
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react"
+import productList from "../../assets/data/products.json"
+import ProductCard from "../ui/ProductCard"
+import "./ProductSection.scss"
 const ProductSection = () => {
+  const [bestItems,setBestItems] = useState([]);
+  useEffect(()=>{
+    // isBest값이 true 상태목록만 저장
+    const items = productList.filter((item)=>{
+      return item.isBest === true;
+    });
+    // slice처리를 해서 베스트 아이템을 8개만.
+    setBestItems(items.slice(0,8));
+  },[]);
   return (
-    <section>ProductSection</section>
+    <section id="product">
+      <h2>BEST SELLERS</h2>
+      <p>가장 사랑받는 베스트 아이템을 만나보세요</p>
+      <div className="product-grid">
+        {
+          bestItems.map((item)=>{
+            return (
+              <ProductCard 
+                key={item.id}
+                item={item}
+              />
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/ui/ProductCard.js
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import "./ProductCard.scss"
+const ProductCard = ({item}) => {
+  const [isHover,setIsHover] = useState(false);
+  const getImagePath = (imgName)=>{
+    let hoverName = imgName;
+    if( isHover ){
+      //경로값에서 -1 --> -2
+      hoverName = imgName.replace('-1','-2');
+    }
+    return require(`../../assets/images/new/${hoverName}`);
+  }
+  return (
+    <div className="product-card"
+      onMouseEnter={()=>{setIsHover(true)}}
+      onMouseLeave={()=>{setIsHover(false)}}
+    >
+      <div className="img-box">
+        <img 
+          src={getImagePath(item.image)}
+          alt={item.title}
+        />
+      </div>
+      <div className="txt-box">
+        <h3>{item.title}</h3>
+        <p>￦{Number(item.price).toLocaleString()}</p>
+      </div>
+      <p className="add-cart">Add Cart</p>
+    </div>
+  )
+}
+
+export default ProductCard
